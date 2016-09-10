1d48c52d44c7e9ba3ba16475925db06298cd3f0c
Jeeva <jeeva.chelladhurai@in.ibm.com>
2016-09-10 10:27:47 -0500

    Dockerfile added, minor code refinement

--- ServerTemplate.py/src/restserver.py
+++ ServerTemplate.py/src/restserver.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, host = '0.0.0.0')
